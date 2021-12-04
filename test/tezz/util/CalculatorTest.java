926deb95741525d14b8364f2fbab359cfb968756
Tejas M R <52408360+tezz-io@users.noreply.github.com>
2021-12-05 00:10:17 +0530

    Update CalculatorTest.java

--- test/tezz/util/CalculatorTest.java
+++ test/tezz/util/CalculatorTest.java
@@ -5,16 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
-  Calculator calc;
-  
-  @BeforeEach
-  public void setUp() {
-    calc = new Calculator();
-  }
-  
   @Test
   @DisplayName("Sample case max element should be found")
   void simpleMaxTest() {
-    assertTrue(10, calc.max({1, 2, 3, 10}));
+    assertTrue(10, Calculator.max(new int[]{1, 2, 3, 10}));
   }
 }
