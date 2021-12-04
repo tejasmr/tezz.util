d7328019730bde4fb6568b8bd4c6868c76250bf6
Tejas M R <52408360+tezz-io@users.noreply.github.com>
2021-12-04 23:38:21 +0530

    Update Calculator.java
    Made maxValue type T and added no element condition for Calculator.max(…)

--- src/tezz/util/Calculator.java
+++ src/tezz/util/Calculator.java
@@ -3,8 +3,10 @@ package tezz.util;
 public class Calculator {
   /* Method that returns the maximum element of an array */
   static <T> T max(T[] arr) {
-    int maxValue = Integer.MIN_VALUE;
-    for( int i=0; i<arr.length; i++ ) {
+    if( arr.length == 0 ) 
+      return 0;
+    T maxValue = arr[0];
+    for( int i=1; i<arr.length; i++ ) {
       if(maxValue < arr[i]) {
         maxValue = arr[i];
       }
