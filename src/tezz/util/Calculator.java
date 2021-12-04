f979c19b962d2c6931306f54f06b5ef37d6a73d0
Tejas M R <52408360+tezz-io@users.noreply.github.com>
2021-12-04 23:43:34 +0530

    Update Calculator.java

--- src/tezz/util/Calculator.java
+++ src/tezz/util/Calculator.java
@@ -2,12 +2,12 @@ package tezz.util;
 
 public class Calculator {
   /* Method that returns the maximum element of an array */
-  static <T> T max(T[] arr) {
+  static <T extends Comparable<T>> T max(T[] arr) {
     if( arr.length == 0 ) 
-      return 0;
+      throw new Exception("Array can not be empty");
     T maxValue = arr[0];
     for( int i=1; i<arr.length; i++ ) {
-      if(maxValue < arr[i]) {
+      if(maxValue.compareTo(arr[i]) < 0 ) {
         maxValue = arr[i];
       }
     }
