ce472032dfc30d9a64041c466feacab0ba5cbacf
Tejas M R <52408360+tezz-io@users.noreply.github.com>
2021-12-06 21:26:50 +0530

    Update CalculatorTest.java

--- src/test/java/tezz/util/CalculatorTest.java
+++ src/test/java/tezz/util/CalculatorTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class CalculatorTest {
 
   @Test
-  void simpleMaxTest() {
+  public void simpleMaxTest() {
     Integer[] arr = new Integer[]{1, 2, 3, 10};
     Integer maxVal = Calculator.max(arr);
     assertTrue("Random sample test", 10 == maxVal);
