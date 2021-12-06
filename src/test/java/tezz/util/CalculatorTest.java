8707f7703bc45a532619e44714c4bff9e028f192
Tejas M R <52408360+tezz-io@users.noreply.github.com>
2021-12-06 21:08:29 +0530

    Update CalculatorTest.java

--- src/test/java/tezz/util/CalculatorTest.java
+++ src/test/java/tezz/util/CalculatorTest.java
@@ -5,9 +5,11 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class CalculatorTest {
+
   @Test
-  @DisplayName("Sample case max element should be found")
   void simpleMaxTest() {
     assertTrue(10, Calculator.max(new int[]{1, 2, 3, 10}));
   }
+
 }
+
