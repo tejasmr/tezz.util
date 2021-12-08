d8276fef91808b13393b9bf2dfeafc34652da658
Tejas M R <52408360+tezz-io@users.noreply.github.com>
2021-12-08 12:12:30 +0530

    Made max function public

--- src/main/java/tezz/util/Calculator.java
+++ src/main/java/tezz/util/Calculator.java
@@ -2,7 +2,7 @@ package tezz.util;
 
 public class Calculator {
   /* Method that returns the maximum element of an array */
-  static <T extends Comparable<T>> T max(T[] arr) {
+  public static <T extends Comparable<T>> T max(T[] arr) {
     T maxValue = arr[0];
     for( int i=1; i<arr.length; i++ ) {
       if(maxValue.compareTo(arr[i]) < 0 ) {
